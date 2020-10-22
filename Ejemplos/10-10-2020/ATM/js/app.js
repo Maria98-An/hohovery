72c71e466502a98dcba3e29c9314ff8d97dcde4c
Israel Huallpara <israelhuallpara@gmail.com>
2020-10-21 22:22:32 -0400

    Hasta almacenar en localstorage

--- Ejemplos/10-10-2020/ATM/js/app.js
+++ Ejemplos/10-10-2020/ATM/js/app.js
@@ -1,4 +1,23 @@
 console.log("Iniciando JS")
+console.info(window.localStorage.getItem("pantallaActual"));
+
+if(window.localStorage.getItem("pantallaActual") == null){
+    mostrarPantalla("screenLenguaje");
+}
+
+function mostrarPantalla(dato){
+    //Ocultando todas las pantallas
+    document.getElementById("screenLenguaje").classList.add("ocultar");
+    document.getElementById("screenPin").classList.add("ocultar");
+
+    //Mostrando pantalla
+    document.getElementById(dato).classList.remove("ocultar");
+    document.getElementById(dato).classList.add("mostrar");
+    //Almacenamos en localstorage la pantalla que fue mostrada.
+    window.localStorage.setItem("pantallaActual", dato);
+}
+
+
 function changeScreen(){
     console.log("Cambiar pantalla");
     //Oculta la pantalla de lenguaje
@@ -10,5 +29,24 @@ function changeScreen(){
 }
 function clicBtn(dato){
     console.log("presiono el btn "+dato);
-    document.getElementById("pin").innerHTML = dato;
+    /*
+    * Validar el registro únicamente de 4 digitos
+    * El boton "Confirmar" únicamente funciona cual el pin tiene 4 dígitos
+    */
+    if(dato == "Cancelar"){
+        document.getElementById("pin").innerHTML = "[ ____ ]"
+    } else if(dato == "Borrar"){
+        let nuevoPin = document.getElementById("pin").innerHTML;
+        if(Number(nuevoPin)>=0) {
+            document.getElementById("pin").innerHTML = (Number(nuevoPin)/10).toFixed(0);
+        }
+    } else {
+        if("[ ____ ]" == document.getElementById("pin").innerHTML){
+            document.getElementById("pin").innerHTML = dato;
+        } else {
+            document.getElementById("pin").innerHTML = document.getElementById("pin").innerHTML + dato
+        }
+    }
+    
+     
 }
