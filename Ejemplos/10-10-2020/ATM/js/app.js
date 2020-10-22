ec7d44698d481c19b095bba988696eb05b32c225
Israel Huallpara <israelhuallpara@gmail.com>
2020-10-21 21:35:54 -0400

    ATM colocando datos a PIN

--- Ejemplos/10-10-2020/ATM/js/app.js
+++ Ejemplos/10-10-2020/ATM/js/app.js
@@ -1,4 +1,14 @@
 console.log("Iniciando JS")
 function changeScreen(){
     console.log("Cambiar pantalla");
+    //Oculta la pantalla de lenguaje
+    document.getElementById("screenLenguaje").classList.remove("mostrar");
+    document.getElementById("screenLenguaje").classList.add("ocultar");
+    //Muestra la pantalla para ingresar PIN
+    document.getElementById("screenPin").classList.remove("ocultar");
+    document.getElementById("screenPin").classList.add("mostrar");
+}
+function clicBtn(dato){
+    console.log("presiono el btn "+dato);
+    document.getElementById("pin").innerHTML = dato;
 }
