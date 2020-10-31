e4ad1d6b18ab39c8d0f888e2571585005d087e30
lesly <ic6697713@gmail.com>
2020-10-31 10:59:13 -0400

    Buenas Practicas

---
+++ ejemplo_Estructuras_index_html/10-10-2020/funciones.js
@@ -0,0 +1,55 @@
+
+
+console.log("Iniciando JS")
+console.info(window.localStorage.getItem("pantallaActual"));
+
+if(window.localStorage.getItem("pantallaActual") == null){
+    mostrarPantalla("screenLenguaje");
+}
+
+function mostrarPantalla(dato){
+    //Ocultando todas las pantallas
+    document.getElementById("screenLenguaje").classList.add("ocultar");
+    document.getElementById("screenPin").classList.add("ocultar");
+
+    //Mostrando pantalla
+    document.getElementById(dato).classList.remove("ocultar");
+    document.getElementById(dato).classList.add("mostrar");
+    //Almacenamos en localstorage la pantalla que fue mostrada.
+    window.localStorage.setItem("pantallaActual", dato);
+}
+
+
+function changeScreen(){
+    console.log("Cambiar pantalla");
+    //Oculta la pantalla de lenguaje
+    document.getElementById("screenLenguaje").classList.remove("mostrar");
+    document.getElementById("screenLenguaje").classList.add("ocultar");
+    //Muestra la pantalla para ingresar PIN
+    document.getElementById("screenPin").classList.remove("ocultar");
+    document.getElementById("screenPin").classList.add("mostrar");
+}
+function clicBtn(dato){
+    console.log("presiono el btn "+dato);
+   
+    /*
+    * Validar el registro únicamente de 4 digitos
+    * El boton "Confirmar" únicamente funciona cual el pin tiene 4 dígitos
+    */
+    if(dato == "Cancelar"){
+        document.getElementById("pin").innerHTML = "[ ____ ]"
+    } else if(dato == "Borrar"){
+        let nuevoPin = document.getElementById("pin").innerHTML;
+        if(Number(nuevoPin)>=0) {
+            document.getElementById("pin").innerHTML = (Number(nuevoPin)/10).toFixed(0);
+        }
+    } else {
+        if("[ ____ ]" == document.getElementById("pin").innerHTML){
+            document.getElementById("pin").innerHTML = dato;
+        } else {
+            document.getElementById("pin").innerHTML = document.getElementById("pin").innerHTML + dato
+        }
+    }
+
+
+}
